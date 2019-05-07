cd00daf6dc887a3dcd054b9e21024da1e57ccfba
billytukija <billytukija@gmail.com>
2019-05-07 12:55:35 -0300

    Create a CRUD MVC in asp.net core using EntityFrameWork
    Create a CRUD MVC in asp.net core using EntityFrameWork

---
+++ CRUD MVC EntityFramework Asp.net Core/mvcNetcore1/mvcNetcore1/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvcNetcore1.Models;
+
+namespace mvcNetcore1.Controllers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            ViewBag.Products = db.Product.ToList(); 
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Add")]
+        public IActionResult Add()
+        {
+            return View("Add", new Product());
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Add(Product product)
+        {
+                db.Add(product);
+                db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("edit")]
+        public IActionResult Edit(int Id)
+        {
+            return View("Edit", db.Product.Find(Id));
+        }
+
+        [HttpPost]
+        [Route("edit")]
+        public IActionResult Edit(Product product)
+        {
+
+            //db.Add(product);
+            db.Entry(product).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public IActionResult Delete(int Id)
+        {
+            db.Remove(db.Product.Find(Id));
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUD MVC EntityFramework Asp.net Core/mvcNetcore1/mvcNetcore1/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcNetcore1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Decimal Price { get; set; }
+        public bool Status { get; set; }
+    }
+}
