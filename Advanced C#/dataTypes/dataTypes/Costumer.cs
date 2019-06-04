a0aec319d03c830eede417e7518728af06ee9ec8
billytukija <billytukija@gmail.com>
2019-06-04 02:27:04 -0300

    Value and  type
    Using  a class StringBuilder to return a formated object

---
+++ Advanced C#/dataTypes/dataTypes/Costumer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataTypes
+{
+    public class Costumer : Object
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Id : " + Id);
+            sb.AppendLine("Name : " + Name);
+            sb.AppendLine("Phone Number : " + PhoneNumber);
+
+            return sb.ToString();
+        }
+    }
+}
--- Advanced C#/dataTypes/dataTypes/Program.cs
+++ Advanced C#/dataTypes/dataTypes/Program.cs
@@ -25,25 +25,39 @@ namespace dataTypes
 
         static void Main(string[] args)
         {
+            var costumer = new Costumer();
+            costumer.Name = "Billy";
+            costumer.Id = 1;
+            costumer.PhoneNumber = 1245454;
+
+            var costumer2 = new Costumer();
+            costumer2 = costumer;
+
+            Console.WriteLine(costumer);
+            Console.WriteLine(costumer2);
+
+            if (costumer.Equals(costumer2))
+                Console.WriteLine("Objects are equal");
+            
             //var x = 10;
             //Bend(ref x);
             //Console.WriteLine(x);
             //Console.Read();
-            
-            DataByRef pos1 = new DataByRef();
+
+            //DataByRef pos1 = new DataByRef();
             //DataByRef pos2 = new DataByRef();
 
-            pos1.X = 30;
+            //pos1.X = 30;
             //pos1.Y = 40;
 
-            Bend(pos1);
+            //Bend(pos1);
 
             //pos2.X = 50;
             //pos2.Y = 60;
 
             //pos1.Y = pos2.Y; // Pointer
 
-            Console.WriteLine("pos1 x :" + pos1.X);
+            //Console.WriteLine("pos1 x :" + pos1.X);
             //Console.WriteLine("pos1 y :" + pos1.Y);
 
             //Console.WriteLine("pos2 x :" + pos2.X);
