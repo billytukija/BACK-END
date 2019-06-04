fa8055fd7cfb85187aedeb059bc33d1c0d240b1d
billytukija <billytukija@gmail.com>
2019-06-04 01:52:35 -0300

    Value and  Reference Type inC#
    Creating a simple project for understand how work value and reference type (C#)

---
+++ Advanced C#/dataTypes/dataTypes/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dataTypes
+{
+    public class Program
+    {
+        public class DataByRef
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        public static void Bend(ref int value)
+        {
+            value = value * 2;
+        }
+
+        public static void Bend(DataByRef pos) {
+            pos.X = pos.X * 2;
+        }
+
+        static void Main(string[] args)
+        {
+            //var x = 10;
+            //Bend(ref x);
+            //Console.WriteLine(x);
+            //Console.Read();
+            
+            DataByRef pos1 = new DataByRef();
+            //DataByRef pos2 = new DataByRef();
+
+            pos1.X = 30;
+            //pos1.Y = 40;
+
+            Bend(pos1);
+
+            //pos2.X = 50;
+            //pos2.Y = 60;
+
+            //pos1.Y = pos2.Y; // Pointer
+
+            Console.WriteLine("pos1 x :" + pos1.X);
+            //Console.WriteLine("pos1 y :" + pos1.Y);
+
+            //Console.WriteLine("pos2 x :" + pos2.X);
+            //Console.WriteLine("pos2 y :" + pos2.Y);
+
+            Console.ReadLine();
+        }
+
+        private static void DataByValue()
+        {
+            var x = 10;
+            var y = 20;
+
+            y = x; // Copy
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.ReadLine();
+        }
+    }
+}
