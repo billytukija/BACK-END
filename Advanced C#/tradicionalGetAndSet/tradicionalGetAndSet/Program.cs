bf975d6422628a8f7bd2327163ff1ce7a05f94b0
billytukija <billytukija@gmail.com>
2019-06-04 05:05:15 -0300

    Update Program.cs
    Adding exception  on trying to debit value more than the balance

--- Advanced C#/tradicionalGetAndSet/tradicionalGetAndSet/Program.cs
+++ Advanced C#/tradicionalGetAndSet/tradicionalGetAndSet/Program.cs
@@ -13,7 +13,8 @@ namespace tradicionalGetAndSet
             private string _client { get; set; }
             private double _balance { get; set; }
 
-            public Account(){
+            public Account()
+            {
                 this._client = "Without name";
                 this._balance = 0.0;
             }
@@ -30,10 +31,14 @@ namespace tradicionalGetAndSet
 
             public double Debit(double value)
             {
+                var res = this._balance - value;
+                if (res < 0)
+                    throw new Exception("Balance is negative");
                 return this._balance;
             }
 
-            public double Credit(double value) {
+            public double Credit(double value)
+            {
                 this._balance = this._balance + value;
                 return this._balance;
             }
@@ -44,7 +49,7 @@ namespace tradicionalGetAndSet
             }
         }
 
-        
+
         static void Main(string[] args)
         {
             Account ac = new Account();
@@ -55,7 +60,7 @@ namespace tradicionalGetAndSet
             ac.Credit(100.0);
             Console.WriteLine(ac.getBalance());
 
-            ac.Debit(500.0);
+            ac.Debit(50.0);
             Console.WriteLine(ac.getBalance());
 
             Console.ReadLine();
