13d909651a31fac04631e07c75e33dc9e655084b
billytukija <billytukija@gmail.com>
2019-06-04 06:08:18 -0300

    Update Program.cs
    Was Added method to get the enum description

--- Advanced C#/tradicionalGetAndSet/tradicionalGetAndSet/Program.cs
+++ Advanced C#/tradicionalGetAndSet/tradicionalGetAndSet/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +10,60 @@ namespace tradicionalGetAndSet
 {
     class Program
     {
+        // Method to retrieve the enum description
+        public static string GetEnumDescription(Enum value)
+        {
+            FieldInfo fi = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute[] attributes =
+                (DescriptionAttribute[])fi.GetCustomAttributes(
+                typeof(DescriptionAttribute),
+                false);
+
+            if (attributes != null &&
+                attributes.Length > 0)
+                return attributes[0].Description;
+            else
+                return value.ToString();
+        }
+
+        enum AccountErrorsMessages
+        {
+            [Description("Balance is negative")]
+            BalanceNegative
+        }
+        
+
+        public class BalanceNegativeException : Exception
+        {
+            public BalanceNegativeException(string exception)
+                : base(exception)
+            {
+            }
+        }
+
         public class Account
         {
             private string _client { get; set; }
-            private double _balance { get; set; }
+            private double _balance;
+
+            public double Balance
+            {
+                get
+                {
+                    return _balance;
+                }
+                set
+                {
+                    if (this._balance < 0)
+                        throw new BalanceNegativeException("");
+                }
+             }
 
             public Account()
             {
                 this._client = "Without name";
-                this._balance = 0.0;
+                this._balance = -1;
             }
 
             public string getClient()
@@ -33,7 +80,7 @@ namespace tradicionalGetAndSet
             {
                 var res = this._balance - value;
                 if (res < 0)
-                    throw new Exception("Balance is negative");
+                    throw new BalanceNegativeException(GetEnumDescription(AccountErrorsMessages.BalanceNegative));
                 return this._balance;
             }
 
@@ -52,16 +99,26 @@ namespace tradicionalGetAndSet
 
         static void Main(string[] args)
         {
-            Account ac = new Account();
-            ac.setClient("Billy");
+            try
+            {
+                Console.WriteLine(GetEnumDescription(AccountErrorsMessages.BalanceNegative));
+                Account ac = new Account();
+                ac.setClient("Billy");
 
-            Console.WriteLine(ac.getClient());
+                Console.WriteLine(ac.getClient());
 
-            ac.Credit(100.0);
-            Console.WriteLine(ac.getBalance());
+                ac.Credit(100.0);
+                Console.WriteLine(ac.getBalance());
 
-            ac.Debit(50.0);
-            Console.WriteLine(ac.getBalance());
+                ac.Debit(500.0);
+                Console.WriteLine(ac.getBalance());
+
+            }
+            catch (BalanceNegativeException bne)
+            {
+                Console.WriteLine(bne.Message);
+            }
+
 
             Console.ReadLine();
         }
