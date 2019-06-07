98ed4667e16f19f11c81457cb94b945d79070a83
billytukija <billytukija@gmail.com>
2019-06-07 18:16:41 -0300

    Using a Dictionary
    Creating a simple project using Dictionary

---
+++ Advanced C#/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Request
+    {
+        public int Number { get; set; }
+        public string Client { get; set; }
+        public double Value { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var requests = new Dictionary<string, Request>();
+
+            var r1 = new Request() { Number = 1, Client = "Billy", Value = 110.55 };
+            var r2 = new Request() { Number = 2, Client = "Richard", Value = 110.55 };
+
+            requests.Add(r1.Client.Substring(0, 3) + r1.Number, r1);
+            requests.Add(r2.Client.Substring(0, 3) + r2.Number, r2);
+
+            foreach (var item in requests.Values)
+                Console.WriteLine(item.Client);
+
+            Console.WriteLine("Chaves e valor do produtos");
+
+            foreach (KeyValuePair<string, Request> item in requests)
+                Console.WriteLine("Key: " + item.Key + " " + " Valor: " + item.Value.Value);
+
+            var sum = requests.Sum(r => r.Value.Value);
+
+            Console.WriteLine("O valor total é : " + sum);
+            Console.ReadLine();
+        }
+    }
+}
