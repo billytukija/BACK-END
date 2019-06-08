cec9759cbfebd551e5826fcf8351eefb523df09c
billytukija <billytukija@gmail.com>
2019-06-07 23:11:30 -0300

    Partial Class
    Simple project using partial classes

---
+++ Advanced C#/PartialClasses/PartialClasses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User("Billy", "123456");
+            if (user.Login())
+                Console.WriteLine("Connected with success");
+            else
+                Console.WriteLine("Incorrect login infos");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Advanced C#/PartialClasses/PartialClasses/User1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public partial class User
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+
+    public User(string userName, string password)
+    {
+        this.UserName = userName;
+        this.Password = password;
+    }
+}
